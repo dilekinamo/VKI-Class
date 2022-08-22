af38bbde0dc5834273bb1de744e075a24b9f1637
dilekinamo <105610438+dilekinamo@users.noreply.github.com>
2022-08-22 03:31:24 +0200

    Add project files.

---
+++ VKIApp.Domain/Hasta.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace VKIApp.Domain
+{
+    public class Hasta
+    {
+     
+        public Hasta()
+        {
+            sayac++;
+            hastaNo = sayac;
+        }
+        private static ushort sayac;
+        public ushort hastaNo;
+        public string hastaAdi;
+        public string hastaSoyadi;
+        public double kilo;
+        public double boy;
+
+        public double VKIHesabi()
+        {
+            return (kilo/((boy/100)*(boy / 100)));
+        }
+        public string VKIDurum(double vki)
+        {
+            if (vki < 25 && vki >= 18.5)
+                return "İDEAL";
+            else if (vki < 30 && vki >= 25)
+                return "HAFİF KİLOLU";
+            else if (vki > 30)
+                return "OBEZ";
+            else if (vki < 18.5)
+                return "ZAYIF";
+            else
+                return "";
+        }
+        public string HastaInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Hasta No: {hastaNo}\n");
+            sb.Append($"Hasta Adı: {hastaAdi}\n");
+            sb.Append($"Hasta Soyadı: {hastaSoyadi}\n");
+            sb.Append($"Hastanın Kilosu: {kilo}\n");
+            sb.Append($"Hastanın Boyu: {boy}\n");
+            sb.Append($"Hastanın Vücut Kitle İndeksi: {Math.Round(VKIHesabi(),2)}\t{VKIDurum(VKIHesabi())}\n");
+
+            return sb.ToString();
+
+        }
+
+    }
+}
---
+++ VKIApp.Presentation.ConsoleUI/Program.cs
@@ -0,0 +1,105 @@
+﻿using VKIApp.Domain;
+
+namespace VKIApp.Presentation.ConsoleUI
+{
+    internal class Program
+    {
+        //public static ushort sayac = 0;
+        public static void Main()
+        {
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Yeni Hasta Kaydı\n2. Hasta Listesi\n3. Hasta Filtrele\n4. Hasta Sil\n5. Cikis");
+            MenuSelection();
+        }
+
+        private static void MenuSelection()
+        {
+            Console.Write("Seçiminiz : ");
+            string choose = Console.ReadLine();
+            switch (choose)
+            {
+                case "1":
+                    YeniHasta();
+                    break;
+                case "2":
+                    HastaListele();
+                    break;
+                case "3":
+                    HastaFiltrele();
+                    break;
+                case "4":
+                    HastaSil();
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    MenuSelection();
+                    break;
+            }
+        }
+
+        private static void HastaSil()
+        {
+            Console.WriteLine("Kacinci siradaki Hasta silinecek?: ");
+            int silIndex= Convert.ToInt32(Console.ReadLine());
+            HastaService.HastayiSil(silIndex);
+            Again();
+        }
+
+        static void Again()
+        {
+            Console.WriteLine("Menüye dönmek için Enter");
+            Console.ReadLine();
+
+            Menu();
+        }
+        private static void HastaFiltrele()
+        {
+            Console.WriteLine("Hasta Soyadının içinde geçen kelimeyi yazın : ");
+            string filterKeyword = Console.ReadLine();
+            var data = HastaService
+                .FilterByChannelName(filterKeyword);
+            PrintList(data);
+        }
+        private static void HastaListele()
+        {
+            var list = HastaService.GetAllHasta();
+            PrintList(list);
+        }
+        static void PrintList(IReadOnlyCollection<Hasta> list)
+        {
+            Console.WriteLine("----------- Liste Başlangıcı ----------");
+            foreach (Hasta h in list)
+            {
+                Console.WriteLine(h.HastaInfo());
+                Console.WriteLine("--------------------------------------");
+            }
+            Console.WriteLine("----------- Liste Sonu ----------");
+            Again();
+        }
+        public static void YeniHasta()
+        {
+            //Console.WriteLine("Hasta No: " + sayac++);
+            Console.WriteLine("Hasta Adı: ");
+            string hastaAdi = Console.ReadLine();
+            Console.WriteLine("Hasta Soyadı: ");
+            string hastaSoyadi = Console.ReadLine();
+            Console.WriteLine("Hastanın Kilosu (kg): ");
+            double kilo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hastanın Boyu (cm): ");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+
+            var data = HastaService.HastaKaydet(hastaAdi,hastaSoyadi,kilo,boy);
+
+            Console.WriteLine($"Hesaplanan VKI Değeri: {Math.Round(data.VKIHesabi(),2)}\t{data.VKIDurum(data.VKIHesabi())}");
+            Again();
+        }
+    }
+}
